1f03e07b9958865abefb63b48edeaad00a4072a5
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-07-05 23:01:05 +0300

    add Best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -19,6 +19,37 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
           ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/test_image.png'),
+                ),
+              ),
+            ),
+          ),
+          Column(children: [
+            
+        ],),
         ],
       ),
     );
