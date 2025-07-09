e2ae7c997df1a56094074d3b73ff6f2793527781
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-07-09 17:15:32 +0300

    add custome book item to book details view body

--- lib/features/home/presentation/views/widgets/custome_book_item.dart
+++ lib/features/home/presentation/views/widgets/custome_book_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class FeaturedListViewItem extends StatelessWidget {
-  const FeaturedListViewItem({super.key});
+class CustomeBookImage extends StatelessWidget {
+  const CustomeBookImage({super.key});
 
   @override
   Widget build(BuildContext context) {
