20d0bc8e028232773066e2e412e1d396ab4f8d0d
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-07-13 22:41:16 +0300

    search view done

--- lib/features/home/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/home/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,7 @@
+import 'package:bookly_app/core/utilities/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:bookly_app/features/home/search/presentation/views/widgets/custome_search_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -9,7 +10,34 @@ class SearchViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: const [CustomeSearchTextField()]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          CustomeSearchTextField(),
+          SizedBox(height: 30),
+          Text('Search Results', style: Styles.textStyle18),
+          SizedBox(height: 16),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: const BookListViewItem(),
+        );
+      },
     );
   }
 }
